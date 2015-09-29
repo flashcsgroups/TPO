84b7fb78a4e1725322828e82e8ce0f739310e766
scrinoman <scrinoman@gmail.com>
2015-09-29 16:29:44 +0300

    Реалировано рекурсивное прохождение по сайту

--- lab6/BrokenLinkDetector.py
+++ lab6/BrokenLinkDetector.py
@@ -2,30 +2,70 @@
 
 import sys
 import urllib
+from urlparse import urlparse
+from urlparse import urljoin
 import socket
+import re
 
 
 failed_pages = {}
 all_pages = {}
+stack = []
+startedNetloc = ''
 
 
-def check_URL( url ):
-    code = urllib.urlopen(url).getcode()
-    all_pages[url] = code
-    if (code not in [200, 301]):
-        failed_pages[url] = code
+def have_protocol( url ):
+    parsedUrl = urlparse(url)
+    if (parsedUrl.scheme in ['https', 'http', 'ftp']):
+        return True
+    return False
+
+
+def check_URL():
+    while (len(stack) > 0):
+        url = stack.pop()
+        try:
+            if (not (urlparse(url).netloc == startedNetloc)):
+                break
+            if (url in all_pages.keys()):
+                break
+            html = urllib.urlopen(url)
+            data = html.read()
+            code = html.getcode()
+            all_pages[url] = code
+            if (code in [200, 301]):
+                urls = []
+                urls.extend(re.findall('<a href="(.+?)".+?>', data, re.DOTALL))
+                for tempUrl in urls:
+                    if (not have_protocol(tempUrl)):
+                        tempUrl = urljoin(url, tempUrl)
+                    if (tempUrl not in all_pages.keys()):
+                        print tempUrl
+                        stack.append(tempUrl)
+            else:
+                failed_pages[url] = code
+        except socket.error, e:
+            failed_pages[url] = 'Ping Error'
     return
 
-    
+
+def perform_output():
+    f = open('report.txt', 'w')
+    for key in all_pages.keys():
+        f.write('{0} - {1}\n'.format(key, all_pages[key]))
+    f.write('\n')
+    for key in failed_pages.keys():
+        f.write('{0} - {1}\n'.format(key, failed_pages[key]))
+
 
 if len(sys.argv) < 2:
     exit()
-starting_URL = sys.argv[1]
-check_URL(starting_URL)
-
-f = open("report.txt", 'w')
-for key in all_pages.keys():
-    f.write("{0} - {1}\n".format(key, all_pages[key]))
-f.write("\n")
-for key in failed_pages.keys():
-    f.write("{0} - {1}\n".format(key, failed_pages[key]))
+if (not have_protocol(sys.argv[1])):
+    print ('Expected absolute link')
+    exit()
+startedNetloc = urlparse(sys.argv[1]).netloc
+print (startedNetloc)
+stack.append(sys.argv[1])
+check_URL()
+perform_output()
+#ошибка в нетлоце который != имени домена
